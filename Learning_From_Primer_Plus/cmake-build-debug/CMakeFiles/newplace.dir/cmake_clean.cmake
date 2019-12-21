3afc6e1175dd5ca0fb2044b8c7495d1a5b080ba4
LinZiYu1996 <2669093302@qq.com>
2019-12-21 23:38:27 +0800

    C

---
+++ Learning_From_Primer_Plus/cmake-build-debug/CMakeFiles/newplace.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/newplace.dir/Chapter_9/9.2/9.2.10/newplace.cpp.o"
+  "libnewplace.dll.a"
+  "newplace.exe"
+  "newplace.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/newplace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
